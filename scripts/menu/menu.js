47bdc68d9f080181d6fca3c21e7900887ead7ad7
Thomahawk98 <thomahawk98@gmail.com>
2026-07-26 03:15:02 -0700

    added credits, improved title screen

--- scripts/menu/menu.js
+++ scripts/menu/menu.js
@@ -25,9 +25,10 @@ class Menu {
         for (const button of this.buttons) {
             button.draw();
         }
+        this.drawCredits();
     }
 
-    drawTitle(x = canvas.width * 0.5, y = 300) {
+    drawTitle(x = canvas.width * 0.5, y = 250) {
         ctx.save();
         ctx.translate(x, y);
         if (user.mouse.inBox(x - 300, y - 50, 600, 100)) ctx.scale(1.1, 1.1); // SKIBIDI but whatever
@@ -37,8 +38,33 @@ class Menu {
         ctx.fillStyle = 'white';
         ctx.textBaseline = 'middle';
         ctx.textAlign = 'center';
-        ctx.f(100);
+        ctx.f(125);
         ctx.fillText(title, 0, 0);
+
+        ctx.restore();
+    }
+
+    drawCredits(x = canvas.width * 0.5, y = canvas.width * 0.5 + 350) {
+        const credits = [
+            'Programming by Thomas Butler',
+            'Artwork by Thomas Butler',
+            'Music by Thomas Butler',
+            'SFX from Pixabay.com',
+        ]
+
+        ctx.save();
+        ctx.translate(x, y);
+
+        ctx.fillStyle = 'rgba(255,255,255,0.5)';
+        ctx.textBaseline = 'middle';
+        ctx.textAlign = 'center';
+        ctx.f(30);
+        for (let n = 0; n < credits.length; n++) {
+            const credit = credits[n];
+            const spacing = 55;
+            const creditsHeight = credits.length * spacing;
+            ctx.fillText(credit, 0, -creditsHeight * 0.5 + spacing * n);
+        }
 
         ctx.restore();
     }
